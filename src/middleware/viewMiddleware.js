c79aaaa89977681fdc474704792f21cfcc3d42d9
ookangzheng <okz5289@gmail.com>
2025-03-16 15:03:33 +0800

    fix: viewCount

--- src/middleware/viewMiddleware.js
+++ src/middleware/viewMiddleware.js
@@ -1,4 +1,5 @@
-exports.setLocals = (req, res, next) => {
+// Define setLocals as a variable first
+const setLocals = (req, res, next) => {
     // Set common locals for all views
     res.locals.user = req.session.user || null;
     res.locals.success_msg = req.flash('success_msg');
@@ -10,4 +11,21 @@ exports.setLocals = (req, res, next) => {
     res.locals.env = process.env;
     
     next();
+};
+
+/**
+ * Ensures that visitCount is defined for all views
+ * This is used as a fallback when the visitCounterMiddleware hasn't run
+ */
+const ensureVisitCount = (req, res, next) => {
+    // Set a default value for visitCount if it's not already defined
+    if (res.locals.visitCount === undefined) {
+        res.locals.visitCount = 0;
+    }
+    next();
+};
+
+module.exports = {
+    setLocals,
+    ensureVisitCount
 };
